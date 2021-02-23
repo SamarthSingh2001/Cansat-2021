bb3090f27efd444c4c16a57f60de5f8830953c89
Josh T <joshliam09@gmail.com>
2021-02-22 23:36:12 -0700

    Minor change to GCS

--- GCS/main.py
+++ GCS/main.py
@@ -9,26 +9,32 @@ command_prefix = "CMD," + str(constants.team_number)
 app = QApplication([])
 #label = QLabel("Team 3226 P.O.P.T.A.R.T.S. Ground Station")
 #label.show()
+
+command_label = QLabel("Command Terminal")
 command_history = QPlainTextEdit()
 command_history.setFocusPolicy(Qt.NoFocus)
 command_input = QLineEdit()
 
 command_layout = QVBoxLayout()
+command_layout.addWidget(command_label)
 command_layout.addWidget(command_history)
 command_layout.addWidget(command_input)
 
 window = QWidget()
 window.setLayout(command_layout)
+window.setWindowTitle("Team 3226 P.O.P.T.A.R.T.S. Ground Station")
 window.show()
 
 # temporary event handlers
 def send_command():
     command = command_input.text()
-    command_history.appendPlainText(command)
-    command_input.clear()
 
-    # the command to be sent to container, WIP
-    print(command_prefix)
+    # only do stuff if there is things in command input
+    if command != "":
+        command_history.appendPlainText(command)
+        command_input.clear()
+        # the command to be sent to container, WIP
+        print(command_prefix)
 
 
 command_input.returnPressed.connect(send_command)
