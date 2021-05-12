852aae69aecd35b1e234bbec9962e1e82e1fbe05
Emil Roy <emiljroy@gmail.com>
2021-05-12 11:25:33 -0700

    updated the update states function

--- GCS/states.py
+++ GCS/states.py
@@ -19,15 +19,15 @@ payloadLabel.setStyleSheet("color: blue;"
                          "border-width: 2px;"
                          "border-color: #1E90FF;"
                          "border-radius: 3px")
-payValidPackets = 2
+payValidPackets = 2 #example values
 payInvalidPackets = 0
 payValidPacketLabel = QLabel("Valid Packets: " + str(payValidPackets))#TODO how do we add packets?
 payInvalidPacketLabel = QLabel("Invalid Packets: " + str(payInvalidPackets))
 
-payload1State = QLabel("Payload 1 State: NOT Deployed")
+payload1State = QLabel("Payload 1 State: Launchpad and Not Deployed")
 payload1State.setStyleSheet("color: red")
 
-payload2State = QLabel("Payload 2 State: NOT Deployed")
+payload2State = QLabel("Payload 2 State: Launchpad and Not Deployed")
 payload2State.setStyleSheet("color: red")
 
 #create container states/statuses
@@ -43,7 +43,7 @@ conInvalidPackets = 2
 conValidPacketLabel = QLabel("Valid Packets: " + str(conValidPackets))#TODO how do we add packets?
 conInvalidPacketLabel = QLabel("Invalid Packets: " + str(conInvalidPackets))
 
-containerState = QLabel("State: Not Deployed")
+containerState = QLabel("State: Launchpad")
 containerState.setStyleSheet("color: red")
 
 # returns layout for payload info
@@ -66,17 +66,32 @@ def buildContainerLayout():
     layout.addWidget(containerState)
     return layout
 
-#function to update if first payload is deployed or not
-def payload1Deployed():
-    payload1State.setText("Payload 1 State: Deployed")
-    payload1State.setStyleSheet("color: green")
+#function to update first payload states
+def updatePayload1State(state):
+    if(state == "deployed"):
+        payload1State.setText("Payload 1 State: Deployed and Descending")
+        payload1State.setStyleSheet("color: green")
 
-#function to update if second payload is deployed or not
-def payload2Deployed():
-    payload2State.setText("Payload 2 State: Deployed")
-    payload2State.setStyleSheet("color: green")
+#function to update if second payload states
+def updatePayload2State(state):
+    if (state == "deployed"):
+        payload2State.setText("Payload 2 State: Deployed and Descending")
+        payload2State.setStyleSheet("color: green")
 
 #function to update container states
-def containerDeployed():
-    containerState.setText("State: Deployed")
-    containerState.setStyleSheet("color: green")
+def updateContainerState(state):
+    if(state == "asending"):
+        containerState.setText("State: Ascending")
+        containerState.setStyleSheet("color: green")
+        payload1State.setText("Payload 1 State: Ascending and NOT Deployed")
+        payload1State.setStyleSheet("color: yellow")
+        payload2State.setText("Payload 2 State: Ascending and NOT Deployed")
+        payload2State.setStyleSheet("color: yellow")
+
+    else if(state == "descending"):
+        containerState.setText("State: Descending")
+        containerState.setStyleSheet("color: yellow")
+    else if(state == "landed"):
+        containerState.setText("State: Landed")
+        containerState.setStyleSheet("color: purple")
+    #TODO what if input wrong cmd?
