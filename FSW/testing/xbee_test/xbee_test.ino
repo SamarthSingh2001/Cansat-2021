d15e2b10af026f6b4c15b547d50fc9a1c3b6e8cf
joshtenorio <joshliam09@gmail.com>
2021-06-06 16:44:39 -0700

    fixing xbee test

--- FSW/testing/xbee_test/xbee_test.ino
+++ FSW/testing/xbee_test/xbee_test.ino
@@ -2,6 +2,7 @@
 #define nss Serial
 
 XBee xbee = XBee();
+Rx16Response rx16 = Rx16Response();
 void setup(){
     Serial.begin(9600);
     Serial1.begin(9600);
@@ -13,7 +14,7 @@ void loop(){
     xbee.readPacket(100);
     if(xbee.getResponse().isAvailable()){
         Serial.println("getting data from gcs");
-        if(Xbee.getResponse().getApiId() == RX_16_RESPONSE){
+        if(xbee.getResponse().getApiId() == RX_16_RESPONSE){
             xbee.getResponse().getRx16Response(rx16);
             for (int i = 0; i < rx.getDataLength(); i++) { 
                 nss.print(rx.getData(i), BYTE); 
