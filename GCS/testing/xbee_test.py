1e0f5dc130d082db073a8a8c46c6b72fe9771d52
joshtenorio <joshliam09@gmail.com>
2021-06-06 11:36:09 -0700

    xbee api mode working

--- GCS/testing/xbee_test.py
+++ GCS/testing/xbee_test.py
@@ -1,4 +1,5 @@
-from digi.xbee.devices import XBeeDevice
+from digi.xbee.devices import XBeeDevice, RemoteXBeeDevice
+from digi.xbee.models.address import XBee64BitAddress
 
 device = XBeeDevice("/dev/ttyUSB0", 9600)
 device.open()
@@ -6,5 +7,12 @@ device.open()
 def callback(message):
     data = message.data.decode("utf8")
     print("packet:" + data)
+    if data == "":
+        print("newline recieved")
 
 device.add_data_received_callback(callback)
+remote = RemoteXBeeDevice(device,
+            XBee64BitAddress.from_hex_string("13A200415CCC25"))
+device.send_data(remote, "CMD,SIMP,3226,15151515\n")
+while True:
+    pass
